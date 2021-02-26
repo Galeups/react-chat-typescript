807932e878fc7d7c58c5932aad4f8701920f4ec7
Galeups <artdevice@gmail.com>
2021-02-26 09:49:08 +0200

    Added simple routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
+import { Route, Redirect, BrowserRouter, Switch } from 'react-router-dom';
 
 import './App.scss';
+import Login from './components/Login/Login';
+import RoomList from './components/RoomList/RoomList';
 
-function App() {
-    return <div className="App">App</div>;
-}
+type SecurityRouteProps = {
+    path: string;
+};
+
+const App: FunctionComponent = () => {
+    const SecureRoute: FunctionComponent<SecurityRouteProps> = ({ children }) => (
+        <BrowserRouter>
+            {localStorage.getItem('nickName') ? children : <Redirect to={{ pathname: '/login' }} />}
+        </BrowserRouter>
+    );
+
+    return (
+        <BrowserRouter>
+            <>
+                <Redirect to={{ pathname: '/room-list' }} />
+
+                <Switch>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+
+                    <SecureRoute path="room-list">
+                        <RoomList />
+                    </SecureRoute>
+                </Switch>
+            </>
+        </BrowserRouter>
+    );
+};
 
 export default App;
